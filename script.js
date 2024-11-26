b10efcdbf7d3593780776348133c7b7e3bd648fc
miiir-nishiym <108381392+miiir-nishiym@users.noreply.github.com>
2024-11-26 10:09:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//戻るボタン
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+
+//TOPページ
 document.addEventListener('DOMContentLoaded', function () {
     const targets = document.getElementsByClassName('tab');
     for (let i = 0; i < targets.length; i++) {
@@ -15,6 +26,28 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementsByClassName('content')[index].classList.add('is-display');
     };
 }, false);
+
+
+//個別ページ
+document.addEventListener('DOMContentLoaded', function () {
+  const targets = document.getElementsByClassName('tab02');
+  for (let i = 0; i < targets.length; i++) {
+      targets[i].addEventListener('click', changeTab, false);
+  }
+  // タブメニューボタンをクリックすると実行
+  function changeTab() {
+      // タブのclassを変更
+      document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+      this.classList.add('is-active');
+      // コンテンツのclassの値を変更
+      document.getElementsByClassName('is-display')[0].classList.remove('is-display');
+      const arrayTabs = Array.prototype.slice.call(targets);
+      const index = arrayTabs.indexOf(this);
+      document.getElementsByClassName('content02')[index].classList.add('is-display');
+  };
+}, false);
+
+
 
 
 
